ced87dd87289772b01efb0a8a50938054c7a8065
EJ Larracas <EJLarracas@outlook.com>
2026-02-14 16:27:46 -0600

    Color

--- out/html/data.js
+++ out/html/data.js
@@ -1,1 +1,13 @@
+const tooltipList = [{
+    searchString: "CCF",
+    explanationText: "<img src=img/ps_a.png> The Co-operative Commonwealth Federation - Left"
+}];
+
+
+const colourList = [{
+        word: "CCF",
+        style: "color: #BA0000; font-weight: bold;",
+        img: "img/ps.png" // this line is not mandatory
+    }
+];
 
